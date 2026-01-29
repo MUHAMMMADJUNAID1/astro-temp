a1ee3965de89b488074d7e065896f686cd48192f
Muhammad Junaid <128121767+MUHAMMMADJUNAID1@users.noreply.github.com>
2026-01-29 10:04:22 +0500

    Update SITE_TITLE for personal portfolio

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jkfy | Personal Portfolio Website';
+export const SITE_TITLE = 'Jkfy | Personal Portfolio ';
 export const SITE_DESCRIPTION = 'Jkdy";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
